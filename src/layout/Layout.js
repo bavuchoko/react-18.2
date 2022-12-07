54d29d5119eb4e6bcff7a4e7049595cd21fb3cc5
pjs <bavuchoko@naver.com>
2022-12-07 22:50:47 +0900

    java

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Header from "../component/layout/Header";
+import {useState} from "react";
+import java from "../assets/images/java.png"
+
 const Layout = () => {
+
+    const [title, setTitle] = useState("awd");
     return (
         <div>
            <Header/>
-            <div className="introduce"></div>
+            <div className="introduce">
+                <img src={java}/>
+                <h2>{title}</h2>
+            </div>
             <main id="top-container">
                 <Outlet />
             </main>
